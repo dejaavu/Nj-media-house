4666e24459c58a58bc8749a84d9053f322d0aa06
Aniket Bhosale <aniket15b@gmail.com>
2019-08-18 18:44:05 +0530

    Added mobile css

--- js/index.js
+++ js/index.js
@@ -5,10 +5,8 @@ function plusDivs(n) {
 }
 
 function showDivs(n) {
-  console.log(slideIndex);
   var i;
   var x = document.getElementsByClassName("mySlides");
-  console.log(x);
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length} ;
   for (i = 0; i < x.length; i++) {
