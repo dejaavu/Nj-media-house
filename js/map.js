868d2d02f810dd5d9715296d7cce6c9c481e8763
tirth-hs <tirthshah200@gmail.com>
2020-11-26 11:47:40 +0530

    Changed the maps to dark

--- js/map.js
+++ js/map.js
@@ -4,7 +4,7 @@ var mymap = L.map('map').setView(center, 15);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoidGlydGgwMzM5IiwiYSI6ImNraHg4YTdtcDF5dmkycG5wanlnaWFpZncifQ.i7-iCnSQO5BaIKavsf28IQ'
